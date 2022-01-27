6c21aece32614f457ba5c522993ce2a5d4af8af3
Paul Bodner <bodner80@gmail.com>
2022-01-27 15:07:23 -0800

    Implamented hashed passwords for security

--- expressServer.js
+++ expressServer.js
@@ -3,37 +3,16 @@ const req = require('express/lib/request');
 const PORT = 8080;
 const app = express();
 const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs')
+
 
 const bodyParser = require("body-parser");
+const bcryptjs = require('bcryptjs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 app.set('view engine', 'ejs');
 
 
-//
-//DATA objects
-//
-
-const urlDatabase = {
-  //shortURL: {
-  //  longURL,
-  //  userID,  
-  //}
-};
-
-const users = {
-  // id: {
-  //  id: alpha Numeric id,
-  //  email: user email,
-  //  password: user password,
-  //  }
-};
-
-
-
-//
-//Relavent functions
-//
 
 const gernerateRandomString = (num) => {
   const alphabet = 'abcdefghijklmnopqrstuvwxyz';
@@ -48,6 +27,43 @@ const gernerateRandomString = (num) => {
   }
   return (randomItems.join(''));
 };
+
+
+
+
+//
+//DATA objects
+//
+
+const newID = gernerateRandomString(4);
+
+const urlDatabase = {
+  //shortURL: {
+  //  longURL,
+  //  userID,  
+  //}
+};
+
+const users = {
+  // id: {
+  //  id: alpha Numeric id,
+  //  email: user email,
+  //  hashedPass: hashed password,
+  //  }
+  TxY76: { 
+    id: 'TxY76',
+    email: 'tool@tool.com',
+    hashedPass: bcrypt.hashSync('tool', 10),
+  }
+};
+
+
+
+//
+//Relavent functions
+//
+
+
 
 const searchUsersByParam = (email, password) => {
   if (!password) {
@@ -67,7 +83,7 @@ const searchUsersByParam = (email, password) => {
 
 const attemptLogin = (email, password) => {
   for (let user in users) {
-    if (users[user].email === email && users[user].password === password) {
+    if (users[user].email === email && users[user].hashedPass === password) {
       return users[user].id;
     }
   }
@@ -99,13 +115,13 @@ app.get('/', (req, res) => {
 //writes database to the screen as a JSON string
 app.get("/urls", (req, res) => {
   const user = users[req.cookies['user_id']];
-  //const alter = users[req.cookies['unathorized-alteration']]
+  console.log(req.cookies['user_id']);
+  console.log(users);
   const templateVars = {
     user,
     urls: urlDatabase,
   };
-  //console.log(templateVars.urls);
-  //console.log(user);
+  
   res.render('urls_index', templateVars);
 });
 
@@ -205,15 +221,20 @@ app.get('/login', (req, res) => {
 app.post('/login', (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
-  //console.log(attemptLogin(email, password));
-
-  if (searchUsersByParam(email, password)) {
-    attemptLogin(email, password) ? res.cookie('user_id', attemptLogin(email, password)) : res.redirect('/login');
-  } else {
-    res.statusCode = 403;
+  //const hashedPass = bcrypt.hashSync(password, 10);
+  //console.log(hashedPass, users);
+  for (let user in users) {
+    //console.log(bcrypt.compareSync(password, users[user].hashedPass));
+    if (users[user].email === email){
+      if (bcrypt.compareSync(password, users[user].hashedPass)){
+        
+        res.cookie('user_id', users[user].id);
+        res.redirect('/urls');
+      }
+    }
   }
-
-  res.redirect('/urls');
+  res.status(403).redirect('/login');
+  
 });
 
 app.post('/logout', (req, res) => {
@@ -237,6 +258,9 @@ app.post('/register', (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
   //console.log(password);
+
+  const hashedPass = bcrypt.hashSync(password, 10);
+
   if (email.length === 0 || password.length === 0) {
     res.statusCode = 400;
     return;
@@ -248,7 +272,7 @@ app.post('/register', (req, res) => {
   users[id] = {
     id,
     email,
-    password,
+    hashedPass,
   };
   res.cookie('user_id', id);
   //console.log(users);
